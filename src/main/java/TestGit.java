6e6bf9b9d9a076e40d29ef15ae06dacea5ec06e6
sunwei199107 <sun_wei199107@163.com>
2018-08-09 00:41:04 +0800

    初始化

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -5,7 +5,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("我是谁");
         System.out.println("我是谁");
-
+        System.out.println("我是谁");
     }
 
 }
